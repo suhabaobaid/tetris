8e6af3218d9e80529d951fe270ea71a2c0ae9b58
suhabaobaid <serenab275@gmail.com>
2017-07-27 21:33:14 +0300

    Initial Commit

---
+++ swiftris/Array2D.swift
@@ -0,0 +1,32 @@
+//
+//  Array2D.swift
+//  swiftris
+//
+//  Created by Suha Baobaid on 27/07/2017.
+//  Copyright © 2017 Suha Baobaid. All rights reserved.
+
+
+class Array2D <T> {
+    
+    let columns: Int
+    let rows: Int
+    
+    var array: Array <T?>
+    
+    init(columns: Int, rows: Int) {
+        self.columns = columns
+        self.rows = rows
+        
+        array = Array<T?>(repeating: nil, count: rows * columns)
+    }
+    
+    subscript(column: Int, row: Int) -> T? {
+        get {
+            return array[(row * columns) + column]
+        }
+        
+        set(newValue){
+            array[(row * columns) + column] = newValue
+        }
+    }
+}
--- swiftris/GameViewController.swift
+++ swiftris/GameViewController.swift
@@ -12,41 +12,23 @@ import GameplayKit
 
 class GameViewController: UIViewController {
 
+    var scene: GameScene!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        if let view = self.view as! SKView? {
-            // Load the SKScene from 'GameScene.sks'
-            if let scene = SKScene(fileNamed: "GameScene") {
-                // Set the scale mode to scale to fit the window
-                scene.scaleMode = .aspectFill
-                
-                // Present the scene
-                view.presentScene(scene)
-            }
-            
-            view.ignoresSiblingOrder = true
-            
-            view.showsFPS = true
-            view.showsNodeCount = true
-        }
-    }
-
-    override var shouldAutorotate: Bool {
-        return true
-    }
-
-    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
-        if UIDevice.current.userInterfaceIdiom == .phone {
-            return .allButUpsideDown
-        } else {
-            return .all
-        }
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Release any cached data, images, etc that aren't in use.
+        //configure the view - as! is a downcaster
+        let skView = view as! SKView
+        skView.isMultipleTouchEnabled = false
+        
+        //create and configure the scene
+        scene = GameScene(size: skView.bounds.size)
+        
+        //aspectFill to cover the whole thing opposite to aspectFit
+        scene.scaleMode = .aspectFill
+        
+        //present the screen
+        skView.presentScene(scene)
     }
 
     override var prefersStatusBarHidden: Bool {
